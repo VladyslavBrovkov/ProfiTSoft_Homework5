953788c3459bfd06fce9633b62e50448b77eb0f8
Vladyslav Brovkov <brovkov777@gmail.com>
2023-01-17 08:42:55 +0200

    Final Commit

--- src/main/java/profitsoft/homework/fifth/model/dto/FilmSearchDto.java
+++ src/main/java/profitsoft/homework/fifth/model/dto/FilmSearchDto.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.DecimalMin;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Jacksonized
 @Builder
 public class FilmSearchDto {
     private int page;
